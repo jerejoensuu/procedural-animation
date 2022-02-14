0c0c0181cc44eb5f463b1b290f25d40bf8a74cc6
Jere Joensuu <jere.joensuu@tuni.fi>
2022-02-14 16:57:58 +0200

    Refactor and document code

--- Assets/Code/BodyManager.cs
+++ Assets/Code/BodyManager.cs
@@ -39,25 +39,45 @@ public class BodyManager : MonoBehaviour {
         CheckQueue();
     }
 
+    /// <summary>
+    /// Apply movement based on player input.
+    /// </summary>
     void ApplyMovement() {
         if (moving) transform.Translate(movementDir * moveSpeed * Time.deltaTime);
         if (rotating) transform.RotateAround(transform.position, Vector3.up, turnSpeed * rotatingDir * Time.deltaTime);
     }
 
+    /// <summary>
+    /// Read player movement input.
+    /// </summary>
+    /// <param name="context"> InputAction event </param>
     void ReadMovement(InputAction.CallbackContext context) {
         moving = context.performed;
         movementDir = new Vector3(context.ReadValue<Vector2>().x, 0, context.ReadValue<Vector2>().y);
     }
 
+    /// <summary>
+    /// Read player rotation input.
+    /// </summary>
+    /// <param name="context"> InputAction event </param>
     void ReadRotation(InputAction.CallbackContext context) {
         rotating = context.performed;
         rotatingDir = (int) context.ReadValue<Vector2>().x;
     }
 
+    /// <summary>
+    /// Check if a leg is found in the currentlyMoving list to see if it should be allowed to step.
+    /// </summary>
+    /// <param name="leg"> Leg GameObject calling the method. </param>
+    /// <returns> Boolean returned as answer. </returns>
     public bool CanStep(GameObject leg) {
         return currentlyMoving.Contains(leg) ? true : false;
     }
 
+    /// <summary>
+    /// Checks if a leg can be moved from the waiting queue to the list of currently moving legs.
+    /// A leg is moved to the list if all legs of the same parity are not moving.
+    /// </summary>
     void CheckQueue() {
         LegController qlCrl;
         LegController lCrl;
@@ -67,7 +87,7 @@ public class BodyManager : MonoBehaviour {
             freeToStep = true;
             foreach (GameObject leg in currentlyMoving) {
                 lCrl = leg.GetComponent<LegController>();
-                if (qlCrl.leg % 2 == lCrl.leg % 2 || qlCrl.side == lCrl.side) {
+                if (qlCrl.leg % 2 == lCrl.leg % 2) {
                     freeToStep = false;
                 }
             }
@@ -84,14 +104,25 @@ public class BodyManager : MonoBehaviour {
         
     }
 
+    /// <summary>
+    /// Adds a leg to the queue of legs waiting to be moved.
+    /// </summary>
+    /// <param name="leg"> Leg waiting to be moved. </param>
     public void JoinQueue(GameObject leg) {
         queue.Add(leg);
     }
 
+    /// <summary>
+    /// Removes leg from list of currently moving legs.
+    /// </summary>
+    /// <param name="leg"> Leg to be removed. </param>
     public void LeaveList(GameObject leg) {
         currentlyMoving.Remove(leg);
     }
 
+    /// <summary>
+    /// To be implemented...
+    /// </summary>
     IEnumerator RestTimer() {
         yield return new WaitForSeconds(1);
         if (moving) { restTimer--; } else { restTimer = 0; }
@@ -102,6 +133,10 @@ public class BodyManager : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Reloads the scene.
+    /// </summary>
+    /// <param name="context"> InputAction event </param>
     void ResetScene(InputAction.CallbackContext context) {
         SceneManager.LoadScene(0);
     }
--- Assets/Code/LegController.cs
+++ Assets/Code/LegController.cs
@@ -6,7 +6,6 @@ public class LegController : MonoBehaviour {
     
     private BodyManager body;
     private Vector3 rayHitPoint;
-    private Vector3 rayPos;
     private Vector3 bodyOrigin;
     public GameObject limbEnd;
     public GameObject steppingArea;
@@ -18,7 +17,7 @@ public class LegController : MonoBehaviour {
     public Side side;
     public int leg;
     Vector3 steppingPoint;
-    Vector3 velocity; // think of a better name
+    Vector3 velocity;
     Vector3 prevPos;
     public Material defaultColor;
     public Material altColor;
@@ -31,7 +30,6 @@ public class LegController : MonoBehaviour {
     void Start() {
         body = transform.parent.GetComponent<BodyManager>();
         rayHitPoint = target.transform.position;
-        SetRayPos();
         bodyOrigin = transform.parent.transform.position;
         Raycast();
         targetOrigin = target.transform.position;
@@ -47,12 +45,14 @@ public class LegController : MonoBehaviour {
             target.transform.position = targetOrigin;
         }
 
-        CalculateAntipedalPoint();
+        // When distance between the leg and stepping area grows big enough, attempt to step.
         if (GetDistance() > maxDistance && !stepping) {
+            // First join the queue for legs wanting to step...
             if (!joinedQueue) {
                 body.JoinQueue(transform.gameObject);
                 joinedQueue = true;
             }
+            // ...Then step when approprite legs are down. (More info in BodyManager)
             if (body.CanStep(transform.gameObject)) {
                 stepping  = true;
                 steppingPoint = GetSteppingPoint();
@@ -63,17 +63,17 @@ public class LegController : MonoBehaviour {
         ChangeColor();
     }
 
-    void SetRayPos() {
-        //rayPos = transform.position + (Vector3.RotateTowards(Vector3.forward, new Vector3((float)side, 0), 100, 100));
-        rayPos = steppingArea.transform.position + (Vector3.up * transform.parent.transform.position.y);
-        //rayPos = Vector3.zero;
-    }
-
+    /// <summary>
+    /// Calculates velocity for the endpoint of the leg.
+    /// </summary>
     void CalculateVelocity() {
         velocity = Vector3.Normalize(transform.position - prevPos);
         prevPos = transform.position;
     }
 
+    /// <summary>
+    /// Casts a ray to move the stepping area.
+    /// </summary>
     void Raycast() {
         RaycastHit hit;
         Vector3 rayStart = steppingArea.transform.position;
@@ -87,22 +87,28 @@ public class LegController : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Moves the leg to new position.
+    /// </summary>
     IEnumerator Step() {
         Vector3 targetPoint = steppingPoint;
+        // Move leg towards new position until it's close enough.
         while(Vector3.Distance(target.transform.position, targetPoint) > 0.6f) {
-            // Vector3 offset = Vector3.RotateTowards(Vector3.forward);
             target.transform.position = Vector3.MoveTowards(target.transform.position, targetPoint, 140f * Time.deltaTime);
-            SineStep(targetPoint);
+            SineStep(targetPoint); // Create stepping motion using sine waves.
             yield return new WaitForSeconds(Time.deltaTime);
         }
         target.transform.position = targetPoint;
         targetOrigin = target.transform.position;
         stepping = joinedQueue = false;
-        body.LeaveList(transform.gameObject);
+        body.LeaveList(transform.gameObject); // Leave the list of currently moving legs.
     }
 
     public void ReturnToRest() { }
 
+    /// <summary>
+    /// Positions the secondary/pole target in front of the leg to point it in the correct direction.
+    /// </summary>
     void AlignLeg() {
         float angle = Vector3.SignedAngle(Vector3.right,
                         new Vector3(target.transform.position.x, 0, target.transform.position.z) - new Vector3(transform.position.x, 0, transform.position.z),
@@ -110,6 +116,10 @@ public class LegController : MonoBehaviour {
         target.transform.eulerAngles = new Vector3(target.transform.eulerAngles.x, angle, target.transform.eulerAngles.z);
     }
 
+    /// <summary>
+    /// Gets distance between leg and the center of the stepping area.
+    /// </summary>
+    /// <returns> Distance between leg and the center of the stepping area </returns>
     float GetDistance() {
         Color color;
         float distance = Vector3.Distance(rayHitPoint, target.transform.position);
@@ -125,29 +135,31 @@ public class LegController : MonoBehaviour {
         return distance;
     }
 
+    /// <summary>
+    /// Gets position for the leg to step to based on its velocity.
+    /// </summary>
+    /// <returns> Position for the leg to step to. </returns>
     Vector3 GetSteppingPoint() {
+        Vector3 center = steppingArea.transform.position;
+        float radius = maxDistance * 0.9f;
+        Vector3 dir = velocity;
+        float angle = Vector3.SignedAngle(Vector3.forward, dir, Vector3.up);
+        Vector3 point = new Vector3(
+            center.x + radius * Mathf.Sin(angle * Mathf.Deg2Rad),
+            0,
+            center.z + radius * Mathf.Cos(angle * Mathf.Deg2Rad) 
+        );
+        Vector3 origin = new Vector3(point.x, body.transform.position.y, point.z);
+
         RaycastHit hit;
-        Vector3 origin = new Vector3(CalculateAntipedalPoint().x, body.transform.position.y, CalculateAntipedalPoint().z);
         Physics.Raycast(origin, -body.transform.up, out hit, 10, ~(1 << 9));
         return hit.point;
     }
 
-    Vector3 CalculateAntipedalPoint() {
-        Vector3 center = steppingArea.transform.position;
-        float radius = maxDistance * 0.9f;
-        Vector3 dir = -velocity;
-        float initDegree = Vector3.SignedAngle(Vector3.forward, dir, Vector3.up);
-        float oppositeAngle = initDegree + 180;
-
-        Vector3 antipedalPoint = new Vector3(
-            center.x + radius * Mathf.Sin(oppositeAngle * Mathf.Deg2Rad),
-            0,
-            center.z + radius * Mathf.Cos(oppositeAngle * Mathf.Deg2Rad) 
-        );
-
-        return antipedalPoint;
-    }
-
+    /// <summary>
+    /// Raises the leg in a stepping motion based on its distance to a target point.
+    /// </summary>
+    /// <param name="targetPoint"> The point the leg is being moved towards. </param>/
     void SineStep(Vector3 targetPoint) {
         float distance = Vector3.Distance(targetPoint, targetOrigin);
         Vector3 targetPos = new Vector3(target.transform.position.x, targetPoint.y, target.transform.position.z);
@@ -155,19 +167,29 @@ public class LegController : MonoBehaviour {
         target.transform.position = new Vector3(target.transform.position.x, targetOrigin.y + y, target.transform.position.z);
     }
 
+    /// <summary>
+    /// Debug method for changing the color of the leg when it's stepping.
+    /// </summary>
     void ChangeColor() {
         transform.GetChild(1).GetChild(1).GetChild(0).GetComponent<MeshRenderer>().material.color = joinedQueue ? Color.red : Color.black;
     }
 
+    /// <summary>
+    /// Used by BodyManager.CheckQueue to allow the leg to pass the queue system if it ends up too far from the stepping area.
+    /// </summary>
+    /// <returns> Distance between leg and leg target </returns>
     public float GetDistanceToTarget() {
         return Vector3.Distance(limbEnd.transform.position, target.transform.position);
     }
 
+    /// <summary>
+    /// Draws gizmos.
+    /// </summary>
     void OnDrawGizmos() {
         Gizmos.color = new Color(1, 0, 0, 0.03f);
         Gizmos.DrawSphere(steppingArea.transform.position, maxDistance);
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(CalculateAntipedalPoint(), 0.3f);
+        Gizmos.DrawSphere(GetSteppingPoint(), 0.3f);
         Gizmos.color = Color.blue;
         Gizmos.DrawSphere(steppingPoint, 0.3f);
         Debug.DrawLine(rayHitPoint + (Vector3.up * 0.1f), limbEnd.transform.position + (Vector3.up * 0.1f), Color.yellow);
